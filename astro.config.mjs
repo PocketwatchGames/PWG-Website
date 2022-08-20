86b5808d767615e75b3f2a4a27e258b542a2a93e
Brian Franco <mastastealth+git@gmail.com>
2022-08-19 22:20:56 -0400

    Or not lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import svelte from "@astrojs/svelte";
 export default defineConfig({
   integrations: [svelte()],
   output: "server",
-  site: "https://PocketwatchGames.github.io",
-  //site: "http://www.pocketwatchgames.com",
+  site: "http://www.pocketwatchgames.com",
 });
