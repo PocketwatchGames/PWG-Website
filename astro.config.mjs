f03d3eeaf39c742934dfbce7f3aa77957a535da9
Brian Franco <mastastealth+git@gmail.com>
2022-08-30 19:47:23 -0400

    Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
+import vercel from "@astrojs/vercel/serverless";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   output: "server",
+  adapter: vercel(),
   site: "http://www.pocketwatchgames.com",
 });
