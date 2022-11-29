ed9f6d525293c61827b313e7ee883b318b5cecee
Brian Franco <mastastealth+git@gmail.com>
2022-11-28 20:11:32 -0500

    Twitch on deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
-  output: "server",
-  adapter: vercel(),
-  site: "http://www.pocketwatchgames.com",
+	integrations: [svelte()],
+	output: "server",
+	adapter: vercel(),
+	site: "http://www.pocketwatchgames.com",
 });
