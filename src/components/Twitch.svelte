df745445793df39273274e52731d18c6fa70536a
Brian Franco <mastastealth+git@gmail.com>
2023-02-03 22:21:25 -0500

    Lazy load iframe proper

--- src/components/Twitch.svelte
+++ src/components/Twitch.svelte
@@ -1,19 +1,29 @@
 <script>
+	import { onMount } from "svelte";
+
 	const parent = import.meta.env.DEV ? "localhost" : "pocketwatchgames.com";
 	const src = `//player.twitch.tv/?channel=pocketwatch&parent=${parent}`;
+	let mounted = false;
+
+	onMount(() => {
+		mounted = true;
+	});
 </script>
 
-<iframe
-	{src}
-	{parent}
-	height="720"
-	width="1280"
-	frameborder="0"
-	scrolling="no"
-	title="Twitch Embed"
-	allowfullscreen="true"
-	loading="lazy"
-/>
+<span />
+{#if mounted}
+	<iframe
+		{src}
+		{parent}
+		height="720"
+		width="1280"
+		frameborder="0"
+		scrolling="no"
+		title="Twitch Embed"
+		allowfullscreen="true"
+		loading="lazy"
+	/>
+{/if}
 
 <style>
 	iframe {
