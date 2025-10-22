902b092855537dd56b34a9ada45af51f94c59e01
Brian Franco <mastastealth+git@gmail.com>
2025-10-22 17:57:39 -0400

    DELETE PLZ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
-import svelte from "@astrojs/svelte";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
